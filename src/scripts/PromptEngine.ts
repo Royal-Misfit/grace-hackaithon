7fbd10ffe070532912651482e71ccd7cb6d66271
AwalAmadou <awala@gracetechnologies.com>
2023-05-17 18:14:36 -0500

    Formic agent prompts created.

--- src/scripts/PromptEngine.ts
+++ src/scripts/PromptEngine.ts
@@ -3,11 +3,19 @@ import { ChatMessage } from "components/RightSidebar/RightSidebar";
 
 const { Configuration, OpenAIApi } = require("openai");
 
-
 interface GptMessage {
   role: "system" | "user" | "assistant";
   content: string;
 }
+
+const formicEnum: string[] = ["G1-Suggestions", "G2-Suggestion-Follow-Up", "G3-Remediation", "G4 Remediation Follow-Up", "G4-Resolution", "G5-Root-Cause-Reporter"];
+const formicPrompts = {
+  "G1-Suggestions":"You are G1-Suggestions, an openai chat completion agent. Users prompt you with machine issues and you interact with them by providing suggestions for possible causes and helping them identify which of the suggestions is the primary cause. Note: Do not stop asking questions until you have reached the final objective of finding the list of suggestions and returning them as an array of strings.",
+  "G2-Suggestion-Follow-Up":"You are G2-Suggestion-Follow-Up, an openai chat completion agent. Users prompt you with an array of suggestions for causes of machine issues. You use this to walk them through the process of identifying which of the suggestions is the primary cause. Note: Do not stop asking questions until you have reached the final objective of finding the primary causes, returned as an array of strings.",
+  "G3-Remediation":"You are G3-Remediation, an openai chat completion agent. Users prompt you with a list of primary causses for machine issues. You use this to walk them through the process of remediating the primary causes. Note: Do not stop asking questions until you have reached the final objective of remediating the primary causes and comfirming with the user that they reply with by saying that the remeditation is complete. After this you reply with a remediation report formatted as a json object.",
+  "G4-Resolution":"You are G4-Resolution, an openai chat completion agent. After users remediate machine issues, users prompt you with a remediation report formatted as a json object. You use this to walk them through the process of confirming the resolution plan. Note: Do not stop asking questions until you have reached the final objective of confirming the remediation plan. Your final output is the resolution plan formatted as a json object.", 
+  "G5-Root-Cause-Reporter":"You are G5-Root-Cause-Reporter, an openai chat completion agent. After users resolve machine issues, users prompt you with 4 items: 1. A list of initial suggestions for what caused the machine issue. This will be in the form of an array of strings. 2. The primary causes, returned as an array of strings. 3. A remediation report formatted as a json object. 4. The resolution plan formatted as a json object. You use those 4 items to generate a concise and easily readable root cause analysis, formatted as a json object."
+};
 
 const executePrompt = async (defectID: string, messageHistory: ChatMessage[], onCompletion: Function) => {
   let temp = "s" + "k-" + btoa("\u001a\u0001­¢´Åvõ\u001fkU\u0098\u000fTàOpen").slice(0, -3) + "k";
@@ -45,6 +53,8 @@ const executePrompt = async (defectID: string, messageHistory: ChatMessage[], on
     });
 
     const completion_text = completion.data.choices[0].message.content;
+
+    ///
     const url = "https://cg-rc-develop.azurewebsites.net/api/AddChatContent?chatDefectID=" + defectID + "&userID=0&chatContent=" + completion_text + "&figureTypeID=0";
     const Http = new XMLHttpRequest();
     Http.open("POST", url);
@@ -66,26 +76,22 @@ const executePrompt = async (defectID: string, messageHistory: ChatMessage[], on
       console.log(error.message);
     }
   }
-  //   }
-  // };
 
-  /*
-const messages = [];
-messages.push({
-    "role": "system",
-        "content": prompt
-    });
-    messages.push({z
-        "role": "user",
-        "content": ""
-    });
-
-    
-    for (const [input_text, completion_text] of history) {
-        messages.push({ role: "user", content: input_text });
-        messages.push({ role: "assistant", content: completion_text });
-    }
-    */
 };
+
+const engageFormicAgent = async (defectID: string, messageHistory: ChatMessage[], onCompletion: Function, formicAgent: string) => {
+  let temp = "s" + "k-" + btoa("\u001a\u0001­¢´Åvõ\u001fkU\u0098\u000fTàOpen").slice(0, -3) + "k";
+  let sk = (temp + btoa("\u0014\u009aÇ\u0018VsÌ(\u009aæ=Êi³/Ü").slice(0, -2)).slice(0, -1) + "G";
+  const configuration = new Configuration({
+    apiKey: sk,
+  });
+  const openai = new OpenAIApi(configuration);
+  
+  // @ts-ignore
+  const prompt: string = formicPrompts[formicAgent];
+  let openAiMessage: GptMessage[] = [{ role: "system", content: prompt }];
+  
+
+}
 
 export default executePrompt;
